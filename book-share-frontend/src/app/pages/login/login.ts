7ce6eb7faa7ebe1cf1b6488b875942ac165732ce
shigal <shiglaws@gmail.com>
2025-07-31 16:06:53 +0530

    added login and register impl v1

---
+++ book-share-frontend/src/app/pages/login/login.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { AuthenticationRequest } from '../../services/models';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/services';
+import { Token } from '../../services/token/token';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+  authRequest: AuthenticationRequest = {email: '', password: ''};
+  errorMsg: Array<String> = [];
+
+  constructor(
+        private router: Router,
+        private authService: AuthenticationService,
+        private tokenService: Token
+  ){}
+
+  login(): void {
+    this.errorMsg = [];
+    this.authService.authenticate({
+      body: this.authRequest
+    }).subscribe({
+      next: (res) => {
+        // todo save token
+        this.tokenService.token = res.token as string;
+        this.router.navigate(['books']);
+      },
+      error: (err) => {
+        console.log(err);
+        if (err.error.validationErrors) {
+          this.errorMsg = err.error.validationErrors
+        } else {
+          this.errorMsg.push(err.error.error);
+        }
+      }
+    })
+  }
+
+  register(){
+    this.router.navigate(['register']);
+  }
+}
---
+++ book-share-frontend/src/app/pages/register/register.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { RegistrationRequest } from '../../services/models';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/services';
+import { error } from 'console';
+
+@Component({
+  selector: 'app-register',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './register.html',
+  styleUrl: './register.scss'
+})
+export class Register {
+  registerRequest: RegistrationRequest = {email: '', firstName: '', lastName: '', password: ''};
+  errorMsg: Array<string> = [];
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ){}
+
+  login(){
+    this.router.navigate(['login']);
+  }
+
+  register(){
+    this.errorMsg = [];
+    this.authService.register({body: this.registerRequest})
+    .subscribe({next: () => {
+      this.router.navigate(['activate-account']);
+    },
+  error: (err) => {
+    this.errorMsg = err.error.validationErrors; 
+  }})
+  }
+}
